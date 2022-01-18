89aec9a526584be7b67e0972f14d1955c08ccf9d
Chris Dekker <chris@krakend.nl>
2022-01-18 16:42:32 +0100

    -- Updated CV -- Updated about image

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -6,7 +6,6 @@ import Tag from "../Components/Tag";
 const About = () => {
     return (
         <div className="about">
-
             <Tag text="About" />
 
             <Lines />
@@ -23,7 +22,7 @@ const About = () => {
                 <p>Hi, my name is Chris Dekker. I'm a 24 year old software engineer & mobile app developer currently living in The Netherlands. Programming has been a great part of my life for some time now, I started my first website when I was 14 years old. With my 5+ years of work experience being a web developer and DevOps engineer I'm currently available for the next challenge. I'm currently aiming for a remote job to expand my horizon. I'd love to work and travel, meet new people, and to learn new things in general. Feel free to contact me to say hi or if you have any questions.</p>
 
                 <div className="actions">
-                    <Button style="default" text="Download my resume" link="/files/Chris_Dekker_-_Software_Engineer_US.pdf" internal={false} />
+                    <Button style="default" text="Download my resume" link="/files/CV_Chris_Dekker_EN.pdf" internal={false} />
                     <Button style="open" text="View my projects" link="/projects" internal={true} />
                 </div>
             </section>
